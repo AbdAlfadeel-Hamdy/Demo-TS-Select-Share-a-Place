0bbd532da85734ca126e88f41355d4d48af9c112
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-03-01 00:09:54 +0200

    rendering a map with openlayers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+declare var ol: any;
+
 const searchAddressHandler = (e: Event) => {
   e.preventDefault();
-  const enteredAddress = addressInput.value;
+  const enteredAddress = addressInput.value.trim();
+  const latlng = enteredAddress.split(",");
+  if (latlng.length !== 2) return alert("Invalid data!");
+
+  addressInput.value = "";
+  const coordinates = { lat: +latlng[0], lng: +latlng[1] }; // Can't fetch coordinates from Google API, use dummy ones
+
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 7,
+    }),
+  });
 };
 
 form.addEventListener("submit", searchAddressHandler);
